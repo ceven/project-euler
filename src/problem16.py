1364d9a45b93890baf1a66acde7bee4f97ac3498
Caroline <carolineven29@gmail.com>
2018-08-25 18:04:40 +1000

    Solved problem 16

--- src/problem16.py
+++ src/problem16.py
@@ -1,4 +1,5 @@
 #
+# Power digit sum
 # 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 # What is the sum of the digits of the number 2^1000?
 #
@@ -6,11 +7,17 @@ import math
 import datetime
 
 
-def p():
+def p(n):
     start_time = datetime.datetime.now()
     # code
+    power = str(int(math.pow(2, n)))
+    sum_digits = 0
+    for c in power:
+        sum_digits += int(c)
     print "Program took {}".format(datetime.datetime.now() - start_time)
+    return sum_digits
 
 
-print p()
+print p(15)
+print p(1000)
 
